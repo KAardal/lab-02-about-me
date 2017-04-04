4999738f295e1fae737c9b5a7cd2ed4b736fbf3a
Kyle Aardal <aardal.kyle@gmail.com>
2017-04-04 14:04:02 -0700

    created function that checks user input.

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 'use strict';
+
+var userResponse = '';
+
+function checkResponse(response) {
+  if(response.trim().toLowerCase() == 'yes' || response.trim().toLowerCase() == 'y') {
+    alert('Correct!');
+    console.log('User was correct.');
+  } else {
+    alert('Wrong!');
+    console.log('User was not correct.');
+  }
+}
+
+userResponse = prompt('Does Kyle like rain drops on roses?');
+console.log('Does Kyle like rain drops on roses?');
+checkResponse(userResponse);
