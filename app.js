3a93d032537b1f6fc66452e63cfb3c9ca3ca9749
Kyle Aardal <aardal.kyle@gmail.com>
2017-04-04 14:19:32 -0700

    created second function to check no aswers. also created four more questions.

--- app.js
+++ app.js
@@ -2,16 +2,42 @@
 
 var userResponse = '';
 
-function checkResponse(response) {
+function checkResponseYes(response) {
   if(response.trim().toLowerCase() == 'yes' || response.trim().toLowerCase() == 'y') {
     alert('Correct!');
-    console.log('User was correct.');
+    console.log('User response: ' + response + '. Correct!');
   } else {
     alert('Wrong!');
-    console.log('User was not correct.');
+    console.log('User response: ' + response + '. Wrong!');
+  }
+}
+
+function checkResponseNo(response) {
+  if(response.trim().toLowerCase() == 'no' || response.trim().toLowerCase() == 'n') {
+    alert('Correct!');
+    console.log('User response: ' + response + '. Correct!');
+  } else {
+    alert('Wrong!');
+    console.log('User response: ' + response + '. Wrong!');
   }
 }
 
 userResponse = prompt('Does Kyle like rain drops on roses?');
 console.log('Does Kyle like rain drops on roses?');
-checkResponse(userResponse);
+checkResponseYes(userResponse);
+
+userResponse = prompt('Does Kyle like whiskers on kittens?');
+console.log('Does Kyle like whiskers on kittens?');
+checkResponseYes(userResponse);
+
+userResponse = prompt('Does Kyle like beer?');
+console.log('Does Kyle like beer?');
+checkResponseYes(userResponse);
+
+userResponse = prompt('Does Kyle like sweet potatoes?');
+console.log('Does Kyle like sweet potatoes?');
+checkResponseNo(userResponse);
+
+userResponse = prompt('Does Kyle like sleep?');
+console.log('Does Kyle like sleep?');
+checkResponseYes(userResponse);
