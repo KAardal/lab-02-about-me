653bc11884895e8318125fc24c245ed00dcd3ace
Kyle Aardal <aardal.kyle@gmail.com>
2017-04-06 15:59:30 -0700

    fixed null crashing prompt, and changed var int to var j

--- app.js
+++ app.js
@@ -37,74 +37,77 @@ function questionOne(){
 }
 questionOne();
 function questionTwo(){
-userResponse = prompt('Does Kyle like whiskers on kittens?');
-console.log('Does Kyle like whiskers on kittens?');
-checkResponseYes(userResponse);
-numQuestions++;
+  userResponse = prompt('Does Kyle like whiskers on kittens?');
+  console.log('Does Kyle like whiskers on kittens?');
+  checkResponseYes(userResponse);
+  numQuestions++;
 }
 questionTwo();
 function questionThree(){
-userResponse = prompt('Does Kyle like beer?');
-console.log('Does Kyle like beer?');
-checkResponseYes(userResponse);
-numQuestions++;
+  userResponse = prompt('Does Kyle like beer?');
+  console.log('Does Kyle like beer?');
+  checkResponseYes(userResponse);
+  numQuestions++;
 }
 questionThree();
 function questionFour(){
-userResponse = prompt('Does Kyle like sweet potatoes?');
-console.log('Does Kyle like sweet potatoes?');
-checkResponseNo(userResponse);
-numQuestions++;
+  userResponse = prompt('Does Kyle like sweet potatoes?');
+  console.log('Does Kyle like sweet potatoes?');
+  checkResponseNo(userResponse);
+  numQuestions++;
 }
 questionFour();
 function questionFive(){
-userResponse = prompt('Does Kyle like sleep?');
-console.log('Does Kyle like sleep?');
-checkResponseYes(userResponse);
-numQuestions++;
+  userResponse = prompt('Does Kyle like sleep?');
+  console.log('Does Kyle like sleep?');
+  checkResponseYes(userResponse);
+  numQuestions++;
 }
 questionFive();
 function questionSix(){
-for(var i = 0; i < 4 && notGuessed; i++){
-  userGuess = prompt('Guess Kyle\'s favorite number.');
-  console.log('Guess Kyle\'s favorite number.');
-  if(userGuess > favoriteNum){
+  for(var i = 0; i < 4 && notGuessed; i++){
+    userGuess = prompt('Guess Kyle\'s favorite number.');
+    console.log('Guess Kyle\'s favorite number.');
+    if(userGuess === null){
+      alert('You hit cancel!');
+      console.log('cancel.');
+    } else if(userGuess > favoriteNum){
       alert('Too high!');
       console.log('Too High!');
-  } else if (userGuess < favoriteNum) {
-    alert('Too low!');
-    console.log('Too low!');
-  } else {
-    alert('You got it!');
-    console.log('You got it!');
-    notGuessed = false;
-    numCorrect++;
+    } else if (userGuess < favoriteNum) {
+      alert('Too low!');
+      console.log('Too low!');
+    } else {
+      alert('You got it!');
+      console.log('You got it!');
+      notGuessed = false;
+      numCorrect++;
+    }
   }
-}
-numQuestions++;
+  numQuestions++;
 }
 questionSix();
 function questionSeven(){
-notGuessed = true;
-for(var i = 0; i < 6 && notGuessed; i++){
-  userGuess = prompt('Guess a Country that Kyle has visited (other than the US).');
-  console.log('Guess a country that kyle has visited');
-  for(var itr = 0; itr < countriesVisited.length; itr++){
-    if(userGuess && userGuess.trim().toLowerCase() === countriesVisited[itr]){
+  notGuessed = true;
+  for(var i = 0; i < 6 && notGuessed; i++){
+    userGuess = prompt('Guess a Country that Kyle has visited (other than the US).');
+    console.log('Guess a country that kyle has visited');
+    for(var j = 0; j < countriesVisited.length; j++){
+      if(userGuess && userGuess.trim().toLowerCase() === countriesVisited[j]){
+        notGuessed = false;
+      }
+    }
+    if(!notGuessed){
+      alert('You guessed it!');
+      console.log('You guessed it!');
       notGuessed = false;
+      numCorrect++;
+    } else {
+      alert('Guess again.');
+      console.log('Guess again.');
     }
   }
-  if(!notGuessed){
-    alert('You guessed it!');
-    console.log('You guessed it!');
-    notGuessed = false;
-    numCorrect++;
-  } else {
-    alert('Guess again.');
-    console.log('Guess again.');
-  }
-}
-numQuestions++;
+  numQuestions++;
 }
 questionSeven();
 alert('You got ' + numCorrect + ' out of' + numQuestions + 'correct');
